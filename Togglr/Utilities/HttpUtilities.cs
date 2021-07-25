d6bed362eca7e635389a826133e2e3c6ac489433
Luke Rushworth <ldjrushworth@icloud.com>
2021-07-25 11:06:20 +0100

    amends to require parameter of type uri

--- Togglr/Utilities/HttpUtilities.cs
+++ Togglr/Utilities/HttpUtilities.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Togglr.Utilities
 {
-
     public static class FetchUtility
     {
         static readonly HttpClient client = new();
-        public static async Task<string> Fetch(string url)
+        
+        public static async Task<string> Fetch(Uri url)
         {
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", "YmZmYjI1NmVhNGE1MmU2ZTM3OGJkYmZkOWU4NDdkYmM6YXBpX3Rva2Vu");
             HttpResponseMessage response = await client.GetAsync(url);
@@ -16,17 +17,4 @@ namespace Togglr.Utilities
             return await response.Content.ReadAsStringAsync();
         }
     }
-    
-    // public class CreateClient
-    // {
-    //     private readonly IHttpClientFactory _clientFactory;
-    //     public CreateClient(IHttpClientFactory clientFactory)
-    //     {
-    //         _clientFactory = clientFactory;
-    //     }
-    //     public static HttpClient NewClient()
-    //     {
-    //         return _clientFactory.CreateClient();
-    //     }
-    // }
 }
