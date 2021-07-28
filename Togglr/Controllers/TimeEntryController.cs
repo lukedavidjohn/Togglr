69df953fd9f25bcb7ab2a69cab8a06330c18a2fb
Luke Rushworth <ldjrushworth@icloud.com>
2021-07-28 18:47:52 +0100

    removes getbyid method, adds getcount and getallsincedate methods

--- Togglr/Controllers/TimeEntryController.cs
+++ Togglr/Controllers/TimeEntryController.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Togglr.Models;
 using Togglr.Services;
@@ -10,7 +12,7 @@ namespace Togglr.Controllers
     [Route("[controller]")]
     public class TimeEntryController : ControllerBase
     {
-        readonly string timeEntryPath = "https://track.toggl.com/api/v9/me/time_entries?since=";
+        readonly string timeEntryPath = "https://track.toggl.com/api/v9/me/time_entries";
 
         public TimeEntryService timeEntryService;
 
@@ -24,52 +26,15 @@ namespace Togglr.Controllers
         {
             return timeEntryService.GetAll();
         }
-        
-        [HttpGet("{id}")]
-        public ActionResult<TimeEntry> Get(int id)
+
+        [HttpGet("~/[controller]/[action]")]
+        public ActionResult<int> GetCount() => timeEntryService.GetCount();
+
+        [HttpGet("{dateString}")]
+        public ActionResult<List<TimeEntry>> GetAllSinceDate(string dateString)
         {
-            var timeEntry = timeEntryService.Get(id);
-            if(timeEntry == null)
-            {
-                return NotFound();
-            }
-            return timeEntry;
+            var sinceDate = DateTime.ParseExact(dateString, "dd-MM-yyyy", new CultureInfo("en-GB"));;
+            return timeEntryService.GetAllSinceDate(sinceDate);
         }
     }
 }
-
-// namespace Togglr.Controllers
-// {
-//     [ApiController]
-//     [Route("[controller]")]
-//     public class TimeEntryController : ControllerBase
-//     {
-//         // public HttpClient _httpClient;
-//         readonly string timeEntryPath = "https://track.toggl.com/api/v9/time_entries";
-//         public TimeEntryService timeEntryService;
-//         public TimeEntryController(HttpClient httpClient)
-//         {
-//             _httpClient = httpClient;
-//             timeEntryService = new TimeEntryService(timeEntryPath);
-//         }
-//         // [HttpPost]
-//         // public async System.Threading.Tasks.Task<IActionResult> Post()
-//         // {
-//         //     HttpResponseMessage response = await _httpClient.GetAsync("https://www.google.co.uk");
-//         //     Console.WriteLine(response);
-//         //     return Ok();
-//         // }
-//         [HttpGet]
-//         public ActionResult<List<TimeEntry>> GetAll() => timeEntryService.GetAll();
-//         [HttpGet("{id}")]
-//         public ActionResult<TimeEntry> Get(int id)
-//         {
-//             var timeEntry = timeEntryService.Get(id);
-//             if(timeEntry == null)
-//             {
-//                 return NotFound();
-//             }
-//             return timeEntry;
-//         }
-//     }
-// }
