880fc3a95df7909166d68bdec70fd73b7e11cfd8
Luke Rushworth <ldjrushworth@icloud.com>
2021-08-04 23:09:44 +0100

    adds setter methods to properties

--- Togglr/Models/TogglData.cs
+++ Togglr/Models/TogglData.cs
@@ -4,9 +4,9 @@ namespace Togglr.Models
 {
     public abstract class TogglData
     {
-        public int Id { get; }
-        public int Wid { get; }
-        public string Name{ get; }
-		public DateTime At{ get; }
+        public int Id { get; set; }
+        public int Wid { get; set; }
+        public string Name{ get; set; }
+		public DateTime At{ get; set; }
     }   
 }
